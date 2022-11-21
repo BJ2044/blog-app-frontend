2e1168c51962c4ef4401862cd1ee1f08580d6753
Brad Jones <BJ2044@gmail.com>
2022-11-21 10:43:07 -0500

    Connected

---
+++ src/pages/addPost.js
@@ -0,0 +1,5 @@
+export default function addPost() {
+  return (
+    <h1> Add Post</h1>
+  )
+}
---
+++ src/pages/singlePost.js
@@ -0,0 +1,5 @@
+export default function singlePost() {
+  return (
+    <h1> Single Post</h1>
+  )
+}
